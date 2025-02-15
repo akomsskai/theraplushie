2d11f1f5644f08412a4ca957aa2020b58cbed672
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-02-15 13:08:52 -0800

    Voice

---
+++ src/voice/src/voice/cli.py
@@ -0,0 +1,15 @@
+import typer
+
+from voice import return_two
+
+app = typer.Typer()
+
+
+@app.command()
+def run():
+    print(f"Here is {return_two()} for you")
+
+
+def entrypoint():
+    app()
+
