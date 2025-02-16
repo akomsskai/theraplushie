6ec093fa334372090d4d700e9a120658411bf01b
Angela Sifuentes <angelabmsifuentes@gmail.com>
2025-02-16 07:02:12 -0700

    audio implementation

--- src/core/src/core/generateResponse.py
+++ src/core/src/core/generateResponse.py
@@ -1,22 +1,30 @@
 import ollama
 from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+import os
+
+load_dotenv()
+ELEVENLABS_API_KEY = os.environ.get("API_KEY")
 
 def getResponse(input):
     print("entered get response")
     output = ollama.chat(model = "theraplushie_7", 
                             messages = [{"role" : "user", "content" : input}])
     print(("ollama ran"))
-    load_dotenv()
-
-    client = ElevenLabs()
+    
+    print(ELEVENLABS_API_KEY)
+    client = ElevenLabs(api_key = ELEVENLABS_API_KEY)
     print(output["message"]["content"])
+    
 
-    """audioFile = client.text_to_speech.convert(
-        text = output
-        voice_id = 5msz3SCgezg6jfzuHiRI
-        model_id = eleven_multilingual_v2
-        output_format = 
+    audioFile = client.text_to_speech.convert(
+        text = output["message"]["content"],
+        voice_id = "5msz3SCgezg6jfzuHiRI",
+        model_id = "eleven_multilingual_v2",
+        output_format = "mp3_44100_64",
     ) 
 
-    return audioFile"""
+    play(audioFile)
+
+    #return audioFile"""
